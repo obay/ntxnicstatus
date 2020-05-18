ae072d3ffe3551d3d0dd17891a5b0b22f3c935e0
Obay <ahmad.obay@gmail.com>
2020-05-18 03:13:16 +0300

    Updating build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,31 @@
 APPNAME=ntxnicstatus
 VERSION=`cat VERSION`
 
+cat /dev/null > "bin/$APPNAME-$VERSION-SHA256SUMS"
+
 export GOOS=linux
 export GOARCH=amd64
-go build -o bin/$VERSION/linux/amd64/$VERSION/$APPNAME
+go build -o bin/$APPNAME
+pushd bin/
+zip "$APPNAME-$VERSION-$GOOS-$GOARCH.zip" $APPNAME
+shasum -a 256 "$APPNAME-$VERSION-$GOOS-$GOARCH.zip" >> "$APPNAME-$VERSION-SHA256SUMS"
+rm -f $APPNAME
+popd
 
 export GOOS=darwin
 export GOARCH=amd64
-go build -o bin/$VERSION/darwin/amd64/$APPNAME
+go build -o bin/$APPNAME
+pushd bin/
+zip "$APPNAME-$VERSION-$GOOS-$GOARCH.zip" $APPNAME
+shasum -a 256 "$APPNAME-$VERSION-$GOOS-$GOARCH.zip" >> "$APPNAME-$VERSION-SHA256SUMS"
+rm -f $APPNAME
+popd
 
 export GOOS=windows
 export GOARCH=amd64
-go build -o bin/$VERSION/windows/amd64/$APPNAME.exe
+go build -o bin/$APPNAME
+pushd bin/
+zip "$APPNAME-$VERSION-$GOOS-$GOARCH.zip" $APPNAME
+shasum -a 256 "$APPNAME-$VERSION-$GOOS-$GOARCH.zip" >> "$APPNAME-$VERSION-SHA256SUMS"
+rm -f $APPNAME
+popd
